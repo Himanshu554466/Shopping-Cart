6fb35dcbf63392b09216910df2206938e7dc639c
Himanshu Tiwari <himanshu554466@gmail.com>
2023-10-06 21:05:53 +0530

    updating count with New item

--- src/itemContext.js
+++ src/itemContext.js
@@ -13,13 +13,22 @@ function CustomItemContext({children}){
     const [item, setItem] = useState(0);
     const [showCart,setShowCart] = useState(false);
 
-    const handleAdd = (price) =>{
-        setTotal((total) => {
-          const newTotal = total + price;
-          console.log(newTotal);
-          return newTotal;
-        });
-        setItem(item+1);
+    // craeted state to store all boject when cart is clicked
+    const [cart,setCart] = useState([]); 
+
+    const handleAdd = (prod) =>{
+        // setTotal((total) => {
+        //   // const newTotal = total + price;
+        //   // console.log(newTotal);
+        //   // return newTotal;
+        // });
+        // setItem(item+1);
+
+        const index = cart.findIndex((item) => item.id === prod.id);
+        if(index === -1){
+          setCart([...cart,{...prod,qty:1}]);
+          setTotal(total + prod.price);
+        }
       };
 
   const handleRemove = (price) => {
@@ -35,7 +44,7 @@ function CustomItemContext({children}){
 
     return(
         <itemContext.Provider value ={{total ,setTotal ,item ,setItem ,handleAdd ,handleRemove,toggle}}>
-          
+
           {/* paasing toggle as aprops */}
           {showCart && <CartModal toggle={toggle} />}
             {children}
